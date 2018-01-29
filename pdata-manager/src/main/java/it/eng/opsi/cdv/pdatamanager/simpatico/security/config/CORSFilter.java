11ef4c7f2eafdcd5e5afeaceaa0d7eb713e07fa6
savarino <savarino>
2018-01-29 16:06:49 +0100

    updated CORS filter conf

--- pdata-manager/src/main/java/it/eng/opsi/cdv/pdatamanager/simpatico/security/config/CORSFilter.java
+++ pdata-manager/src/main/java/it/eng/opsi/cdv/pdatamanager/simpatico/security/config/CORSFilter.java
@@ -13,12 +13,12 @@ public class CORSFilter implements ContainerResponseFilter {
     @Override
     public void filter(ContainerRequestContext request,
             ContainerResponseContext response) throws IOException {
-        response.getHeaders().add("Access-Control-Allow-Origin", "*");
-        response.getHeaders().add("Access-Control-Allow-Headers",
-                "*");
-        response.getHeaders().add("Access-Control-Allow-Credentials", "true");
-        response.getHeaders().add("Access-Control-Allow-Methods",
-                "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+       // response.getHeaders().add("Access-Control-Allow-Origin", "*");
+        //response.getHeaders().add("Access-Control-Allow-Headers",
+          //      "*");
+        //response.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        //response.getHeaders().add("Access-Control-Allow-Methods",
+          //      "GET, POST, PUT, DELETE, OPTIONS, HEAD");
         
     }
 }
