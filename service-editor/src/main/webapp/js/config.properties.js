07a6944530c7acf6fcd7e9e03a313c42d46ce92c
@2038GE <elefante@2038.it>
2018-11-12 11:04:57 +0100

    config.properties and minor integrations

---
+++ service-editor/src/main/webapp/js/config.properties.js
@@ -0,0 +1,8 @@
+var app_parameters={
+		host_param: 'localhost',
+		port_param: '8080',
+		findById_apipath: 'service-manager/api/v1/services/',
+		update_apipath: 'service-manager/api/v1/services/',
+		create_apipath: 'service-manager/api/v1/services/',
+		getServices_apipath: 'service-manager/api/v1/services/'
+};
